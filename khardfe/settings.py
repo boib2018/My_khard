fc7e152c99b2b0c3504a00eb74b785a5b4b3389e
boib <boibmichael8@gmail.com>
2025-06-05 19:45:32 +0000

    Your commit message

--- khardfe/settings.py
+++ khardfe/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = 'django-insecure--qfv12sr1ug089i6#dp$!s7e9g^ipph2*2mn*jw#xl&frf#=@m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'mkhard.onrender.com',
+]
 
 
 # Application definition
